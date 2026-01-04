ecf01fb18eb9c39818fe95e161f01f3cc6d9774b
Realitaa <rezamuliaputra927@gmail.com>
2026-01-04 10:27:12 +0700

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/hints',
+  ],
+  devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  compatibilityDate: '2024-04-03',
+})
