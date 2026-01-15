0e3fe0d88fe642faae700187e6af2f543125adf0
Realitaa <rezamuliaputra927@gmail.com>
2026-01-15 08:47:32 +0700

    feat: add custom error page

---
+++ app/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps<{ error: NuxtError }>()
+</script>
+
+<template>
+  <NuxtLayout>
+    <div class="flex flex-col gap-4 min-h-[calc(100vh-4rem)] w-full items-center justify-center text-center">
+      <h1 class="text-4xl font-bold">{{ error.statusCode }}</h1>
+      <h2 class="text-xl font-semibold">{{ error.statusMessage }}</h2>
+      <NuxtLink to="/" class="inline-flex items-center gap-2 text-sm font-medium text-primary">Go back home</NuxtLink>
+    </div>
+  </NuxtLayout>
+</template>
