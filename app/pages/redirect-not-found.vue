9d0f2187cbbe3817cba893586374eb9e222a9fc8
Realitaa <rezamuliaputra927@gmail.com>
2026-02-05 11:17:42 +0700

    feat: add redirect-not-found for go.realitaa.dev

---
+++ app/pages/redirect-not-found.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useError } from "#app";
+
+// Set the meta for the page
+definePageMeta({
+  layout: "default",
+});
+
+// Trigger the error
+const error = useError();
+error.value = {
+  statusCode: 404,
+  statusMessage:
+    "You were redirected here because you tried to access a link that was not found in the link shortener <a href='https://go.realitaa.dev' class='text-primary'>https://go.realitaa.dev</a>",
+};
+</script>
+
+<template>
+  <div>
+    <!-- This page will redirect to the error layout -->
+  </div>
+</template>
